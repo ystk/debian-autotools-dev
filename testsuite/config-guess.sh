d95d8c956109cb8b7c39f00802a923a6b361b067
Skerlet Development Team <skerlet@swc.toshiba.co.jp>
2014-08-28 18:36:27 +0900

    git-debimport autotools-dev_20140510.1.tar.xz

--- testsuite/config-guess.sh
+++ testsuite/config-guess.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 #
-# Copyright 2004, 2005, 2009 Free Software Foundation, Inc.
+# Copyright 2004, 2005, 2009, 2012 Free Software Foundation, Inc.
 # Contributed by Ben Elliston <bje@gnu.org>.
 #
 # This test reads 5-tuples from config-guess.data: the components of
 # the simulated uname(1) output and the expected GNU system triplet.
 
 verbose=false
-export PATH=`pwd`:$PATH
+PATH=`pwd`:$PATH
 
 run_config_guess ()
 {
@@ -17,7 +17,15 @@ run_config_guess ()
 	    -e "s,@MACHINE@,$machine," \
 	    -e "s,@RELEASE@,$release," \
 	    -e "s,@SYSTEM@,$system," \
-	    -e "s,@VERSION@,$version," < uname.in > uname
+	    -e "s,@VERSION@,$version," > uname << EOF
+#!/bin/sh
+[ \$# -ne 1 ] && exec sh \$0 -s
+[ \$1 = -m ] && echo "@MACHINE@" && exit 0
+[ \$1 = -r ] && echo "@RELEASE@" && exit 0
+[ \$1 = -s ] && echo "@SYSTEM@" && exit 0
+[ \$1 = -v ] && echo "@VERSION@" && exit 0
+[ \$1 = -p ] && echo "Pentium III(TM)-ISA/PCI"
+EOF
 	chmod +x uname
 	output=`sh ../config.guess 2>/dev/null`
 	if test $? != 0 ; then
